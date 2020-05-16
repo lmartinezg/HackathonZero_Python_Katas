7b4f0b56f09ccc395538fb376bc176d8ff43bd78
Lorenzo Martinez <lmartinezg_hm@hotmail.com>
2020-05-16 12:00:47 +0200

    Added folder and main.py for each kata

---
+++ Kata1/main.py
@@ -0,0 +1,26 @@
+nombre = 'Nombre Apellidos'
+edad = 25
+esta_Trabajando = True
+
+# Comentario de línea
+
+""" 
+Comentario en bloque
+ """
+
+
+dato = input('Introduce un dato: ')
+
+print(dato)
+
+
+edad = edad + 1
+print('Edad: ', edad)
+
+if(edad >= 18):
+    nombre2 = input('Tu nombre: ')
+    print(nombre2 + ', eres mayor de edad')
+else:
+    print('Eres un crio')
+
+ 
